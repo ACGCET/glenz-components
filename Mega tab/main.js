92758128f37e522fbca99e1e21840025fbc5dac3
Saravana <saravanakumar.k@doodleblue.com>
2021-05-16 21:30:13 +0530

    Mega-tab

---
+++ Mega tab/main.js
@@ -0,0 +1,203 @@
+const one = () =>
+{
+document.getElementById('one').style.display='block';
+document.getElementById('two').style.display='none';
+document.getElementById('three').style.display='none';
+document.getElementById('four').style.display='none';
+document.getElementById('five').style.display='none';
+document.getElementById('six').style.display='none';
+document.getElementById('elementor-tab-title1').style.borderBottom = "2px solid #FEC200";
+document.getElementById('tab1').style.background ="#FEC200";
+document.getElementById('img1').style. opacity = "1";
+document.getElementById('tit1').style.color = "#000";
+document.getElementById('elementor-tab-title2').style.borderBottom = "";
+document.getElementById('tab2').style.background ="#F5F6F8";
+document.getElementById('img2').style. opacity = "0.3";
+document.getElementById('tit2').style.color = "#383838";
+document.getElementById('elementor-tab-title3').style.borderBottom = "";
+document.getElementById('tab3').style.background ="#F5F6F8";
+document.getElementById('img3').style. opacity = "0.3";
+document.getElementById('tit3').style.color = "#383838";
+document.getElementById('elementor-tab-title4').style.borderBottom = "";
+document.getElementById('tab4').style.background ="#F5F6F8";
+document.getElementById('img4').style. opacity = "0.3";
+document.getElementById('tit4').style.color = "#383838";
+document.getElementById('elementor-tab-title5').style.borderBottom = "";
+document.getElementById('tab5').style.background ="#F5F6F8";
+document.getElementById('img5').style. opacity = "0.3";
+document.getElementById('tit5').style.color = "#383838";
+document.getElementById('elementor-tab-title6').style.borderBottom = "";
+document.getElementById('tab6').style.background ="#F5F6F8";
+document.getElementById('img6').style. opacity = "0.3";
+document.getElementById('tit6').style.color = "#383838";
+
+}
+const two = ()=>
+{
+document.getElementById('one').style.display='none';
+document.getElementById('two').style.display='block';
+document.getElementById('three').style.display='none';
+document.getElementById('four').style.display='none';
+document.getElementById('five').style.display='none';
+document.getElementById('six').style.display='none';
+document.getElementById('elementor-tab-title2').style.borderBottom = "2px solid #FEC200";
+document.getElementById('tab2').style.background ="#FEC200";
+document.getElementById('img2').style. opacity = "1";
+document.getElementById('tit2').style.color = "#000";
+document.getElementById('elementor-tab-title1').style.borderBottom = 'none';
+document.getElementById('tab1').style.background ="#F5F6F8";
+document.getElementById('img1').style. opacity = "0.3";
+document.getElementById('tit1').style.color = "#383838";
+document.getElementById('elementor-tab-title3').style.borderBottom = "";
+document.getElementById('tab3').style.background ="#F5F6F8";
+document.getElementById('img3').style. opacity = "0.3";
+document.getElementById('tit3').style.color = "#383838";
+document.getElementById('elementor-tab-title4').style.borderBottom = "";
+document.getElementById('tab4').style.background ="#F5F6F8";
+document.getElementById('img4').style. opacity = "0.3";
+document.getElementById('tit4').style.color = "#383838";
+document.getElementById('elementor-tab-title5').style.borderBottom = "";
+document.getElementById('tab5').style.background ="#F5F6F8";
+document.getElementById('img5').style. opacity = "0.3";
+document.getElementById('tit5').style.color = "#383838";
+document.getElementById('elementor-tab-title6').style.borderBottom = "";
+document.getElementById('tab6').style.background ="#F5F6F8";
+document.getElementById('img6').style. opacity = "0.3";
+document.getElementById('tit6').style.color = "#383838";
+		
+}
+const three = ()=>
+{
+document.getElementById('one').style.display='none';
+document.getElementById('two').style.display='none';
+document.getElementById('three').style.display='block';
+document.getElementById('four').style.display='none';
+document.getElementById('five').style.display='none';
+document.getElementById('six').style.display='none';
+document.getElementById('elementor-tab-title3').style.borderBottom = "2px solid #FEC200";
+document.getElementById('tab3').style.background ="#FEC200";
+document.getElementById('img3').style. opacity = "1";
+document.getElementById('tit3').style.color = "#000";
+document.getElementById('elementor-tab-title2').style.borderBottom = "";
+document.getElementById('tab2').style.background ="#F5F6F8";
+document.getElementById('img2').style. opacity = "0.3";
+document.getElementById('tit2').style.color = "#383838";
+document.getElementById('elementor-tab-title1').style.borderBottom = 'none';
+document.getElementById('tab1').style.background ="#F5F6F8";
+document.getElementById('img1').style. opacity = "0.3";
+document.getElementById('tit1').style.color = "#383838";
+document.getElementById('elementor-tab-title4').style.borderBottom = "";
+document.getElementById('tab4').style.background ="#F5F6F8";
+document.getElementById('img4').style. opacity = "0.3";
+document.getElementById('tit4').style.color = "#383838";
+document.getElementById('elementor-tab-title5').style.borderBottom = "";
+document.getElementById('tab5').style.background ="#F5F6F8";
+document.getElementById('img5').style. opacity = "0.3";
+document.getElementById('tit5').style.color = "#383838";
+document.getElementById('elementor-tab-title6').style.borderBottom = "";
+document.getElementById('tab6').style.background ="#F5F6F8";
+document.getElementById('img6').style. opacity = "0.3";
+document.getElementById('tit6').style.color = "#383838";
+}
+ const four = ()=>
+{
+document.getElementById('one').style.display='none';
+document.getElementById('two').style.display='none';
+document.getElementById('three').style.display='none';
+document.getElementById('four').style.display='block';
+document.getElementById('five').style.display='none';
+document.getElementById('six').style.display='none';
+document.getElementById('elementor-tab-title4').style.borderBottom = "2px solid #FEC200";
+document.getElementById('tab4').style.background ="#FEC200";
+document.getElementById('img4').style. opacity = "1";
+document.getElementById('tit4').style.color = "#000";
+document.getElementById('elementor-tab-title2').style.borderBottom = "";
+document.getElementById('tab2').style.background ="#F5F6F8";
+document.getElementById('img2').style. opacity = "0.3";
+document.getElementById('tit2').style.color = "#383838";
+document.getElementById('elementor-tab-title3').style.borderBottom = "";
+document.getElementById('tab3').style.background ="#F5F6F8";
+document.getElementById('img3').style. opacity = "0.3";
+document.getElementById('tit3').style.color = "#383838";
+document.getElementById('elementor-tab-title1').style.borderBottom = 'none';
+document.getElementById('tab1').style.background ="#F5F6F8";
+document.getElementById('img1').style. opacity = "0.3";
+document.getElementById('tit1').style.color = "#383838";
+document.getElementById('elementor-tab-title5').style.borderBottom = "";
+document.getElementById('tab5').style.background ="#F5F6F8";
+document.getElementById('img5').style. opacity = "0.3";
+document.getElementById('tit5').style.color = "#383838";
+document.getElementById('elementor-tab-title6').style.borderBottom = "";
+document.getElementById('tab6').style.background ="#F5F6F8";
+document.getElementById('img6').style. opacity = "0.3";
+document.getElementById('tit6').style.color = "#383838";
+		
+}
+ const five = ()=>
+{
+document.getElementById('one').style.display='none';
+document.getElementById('two').style.display='none';
+document.getElementById('three').style.display='none';
+document.getElementById('four').style.display='none';
+document.getElementById('five').style.display='block';
+document.getElementById('six').style.display='none';
+document.getElementById('elementor-tab-title5').style.borderBottom = "2px solid #FEC200";
+document.getElementById('tab5').style.background ="#FEC200";
+document.getElementById('img5').style. opacity = "1";
+document.getElementById('tit5').style.color = "#000";
+document.getElementById('elementor-tab-title2').style.borderBottom = "";
+document.getElementById('tab2').style.background ="#F5F6F8";
+document.getElementById('img2').style. opacity = "0.3";
+document.getElementById('tit2').style.color = "#383838";
+document.getElementById('elementor-tab-title3').style.borderBottom = "";
+document.getElementById('tab3').style.background ="#F5F6F8";
+document.getElementById('img3').style. opacity = "0.3";
+document.getElementById('tit3').style.color = "#383838";
+document.getElementById('elementor-tab-title4').style.borderBottom = "";
+document.getElementById('tab4').style.background ="#F5F6F8";
+document.getElementById('img4').style. opacity = "0.3";
+document.getElementById('tit4').style.color = "#383838";
+document.getElementById('elementor-tab-title1').style.borderBottom = 'none';
+document.getElementById('tab1').style.background ="#F5F6F8";
+document.getElementById('img1').style. opacity = "0.3";
+document.getElementById('tit1').style.color = "#383838";
+document.getElementById('elementor-tab-title6').style.borderBottom = "";
+document.getElementById('tab6').style.background ="#F5F6F8";
+document.getElementById('img6').style. opacity = "0.3";
+document.getElementById('tit6').style.color = "#383838";
+		
+}
+const six = ()=>
+{
+document.getElementById('one').style.display='none';
+document.getElementById('two').style.display='none';
+document.getElementById('three').style.display='none';
+document.getElementById('four').style.display='none';
+document.getElementById('five').style.display='none';
+document.getElementById('six').style.display='block';
+document.getElementById('elementor-tab-title6').style.borderBottom = "2px solid #FEC200";
+document.getElementById('tab6').style.background ="#FEC200";
+document.getElementById('img6').style. opacity = "1";
+document.getElementById('tit6').style.color = "#000";
+document.getElementById('elementor-tab-title2').style.borderBottom = "";
+document.getElementById('tab2').style.background ="#F5F6F8";
+document.getElementById('img2').style. opacity = "0.3";
+document.getElementById('tit2').style.color = "#383838";
+document.getElementById('elementor-tab-title3').style.borderBottom = "";
+document.getElementById('tab3').style.background ="#F5F6F8";
+document.getElementById('img3').style. opacity = "0.3";
+document.getElementById('tit3').style.color = "#383838";
+document.getElementById('elementor-tab-title4').style.borderBottom = "";
+document.getElementById('tab4').style.background ="#F5F6F8";
+document.getElementById('img4').style. opacity = "0.3";
+document.getElementById('tit4').style.color = "#383838";
+document.getElementById('elementor-tab-title5').style.borderBottom = "";
+document.getElementById('tab5').style.background ="#F5F6F8";
+document.getElementById('img5').style. opacity = "0.3";
+document.getElementById('tit5').style.color = "#383838";
+document.getElementById('elementor-tab-title1').style.borderBottom = 'none';
+document.getElementById('tab1').style.background ="#F5F6F8";
+document.getElementById('img1').style. opacity = "0.3";
+document.getElementById('tit1').style.color = "#383838";
+		
+}
